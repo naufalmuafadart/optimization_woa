3ec956a4d1a26628e03546caa3619973912efc7e
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2024-04-10 05:17:34 +0700

    add BA testing

--- ba.py
+++ ba.py
@@ -5,12 +5,26 @@ from vns import VNS
 import copy
 import random
 import math
-
 import numpy as np
+from mealpy import FloatVar, BA
+
+# def objective_function(solution):
+#     return np.sum(solution**2)
+#
+# problem_dict = {
+#     "bounds": FloatVar(lb=[-100, ] * 30, ub=[100, ]*30),
+#     "obj_func": objective_function,
+#     "minmax": "min",
+# }
+#
+# model = BA.AdaptiveBA(epoch=1000, pop_size=50, loudness_min=1.0, loudness_max=2.0, pr_min=0.1, pr_max= 0.85, pf_min = -9.9, pf_max = 10.)
+# g_best = model.solve(problem_dict)
+# print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+# print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
 
 class WOA_VRP:
-    AGENT_COUNT = 2
-    MAX_ITERATIONS = 1
+    AGENT_COUNT = 10
+    MAX_ITERATIONS = 10
 
     def __init__(self, ids, hotel_id, doi_duration, doi_cost, doi_rating, days_count):
         self.PREFERENCE_ID = list(map(int, ids)) # => [1,2,3]
@@ -18,7 +32,7 @@ class WOA_VRP:
         self.DOI_DURATION = doi_duration
         self.DOI_COST = doi_cost
         self.DOI_RATING = doi_rating
-        self.DOI_INCLUDED_RANGE = 1
+        self.DOI_POI_INCLUDED = 1
         self.DAYS_COUNT = days_count
         self.AGENT_LENGTH = len(self.PREFERENCE_ID)
         self.DEPART_TIME = 8 * 3600
@@ -210,7 +224,7 @@ class WOA_VRP:
         MAUT += self.get_v(duration, self.DURATION_RANGE[0], self.DURATION_RANGE[1], False) * self.DOI_DURATION
         MAUT += self.get_v(rating, self.RATING_RANGE[0], self.RATING_RANGE[1], True) * self.DOI_RATING
         MAUT += self.get_v(costs, self.COST_RANGE[0], self.COST_RANGE[1], False) * self.DOI_COST
-        MAUT += self.get_v(len(assigned_ids), self.POI_INCLUDED_RANGE[0], self.POI_INCLUDED_RANGE[1], True) * self.DOI_INCLUDED_RANGE
+        MAUT += self.get_v(len(assigned_ids), self.POI_INCLUDED_RANGE[0], self.POI_INCLUDED_RANGE[1], True) * self.DOI_POI_INCLUDED
         return MAUT
 
     def get_best_agent(self, agents):
@@ -336,7 +350,9 @@ class WOA_VRP:
 
     def construct_solution(self):
         # get dataset
-        self.prepare_dataset()
+        self.df_places = DatasetProvider.get_places()
+        self.df_time_matrix = DatasetProvider.get_time_matrix()
+        self.df_schedule = DatasetProvider.get_schedule()
 
         # generate initial population
         agents = self.generate_initial_population(self.AGENT_COUNT, self.AGENT_LENGTH)
@@ -348,8 +364,20 @@ class WOA_VRP:
         self.POI_INCLUDED_RANGE = [self.DAYS_COUNT, len(self.PREFERENCE_ID)]
         self.FITNESS_VALUE_RANGE = [1.5, 2.3]
 
-        agents_ = copy.deepcopy(agents)
-        Xbest, Fbest = self.WOA(0.0, 10.0, agents_)
+        problem_dict = {
+            "bounds": FloatVar(lb=(0.,) * len(self.PREFERENCE_ID), ub=(10.,) * len(self.PREFERENCE_ID), name="delta"),
+            "obj_func": self.fitness_function,
+            "minmax": "max",
+        }
+        model = BA.OriginalBA(epoch=5, pop_size=50, loudness=0.8, pulse_rate=0.95, pf_min=0.1, pf_max=10.0)
+        g_best = model.solve(problem_dict)
+        Xbest = g_best.solution
+        Fbest = g_best.target.fitness
+        print(f"Solution: {g_best.solution}, Fitness: {g_best.target.fitness}")
+        print(f"Solution: {model.g_best.solution}, Fitness: {model.g_best.target.fitness}")
+
+        # agents_ = copy.deepcopy(agents)
+        # Xbest, Fbest = self.WOA(0.0, 10.0, agents_)
         route = self.greedy_separate_route(Xbest, self.PREFERENCE_ID, self.DAYS_COUNT)
 
         fitness_value = self.fitness_function(Xbest)
@@ -377,95 +405,8 @@ class WOA_VRP:
             })
 
         normalized_fitness = (fitness_value - self.FITNESS_VALUE_RANGE[0]) / (self.FITNESS_VALUE_RANGE[1] - self.FITNESS_VALUE_RANGE[0])
-        return output['results'], normalized_fitness
-
-
-class WOA_VNS_VRP(WOA_VRP):
-    def __init__(self, ids, hotel_id, doi_duration, doi_cost, doi_rating, days_count):
-        super().__init__(ids, hotel_id, doi_duration, doi_cost, doi_rating, days_count)
-
-    def WOA(self, min_x, max_x, agents):
-        # setup VNS
-        vns = VNS(1, self.fitness_function, True)
-
-        t = 0
-
-        # Fbest : nilai fitness terbaik
-        # Xbest : agen terbaik
-        Fbest, Xbest = self.get_best_agent(agents)
-        agent_dimension = int(len(Xbest) / self.DAYS_COUNT) # banyak tempat wisata sesuai preferensi user
-        fitness_values = []
-
-        # Menyimpan nilai fitness untuk setiap agen
-        for i in range(len(agents)):
-            fitness_values.append(self.fitness_function(agents[i]))
-
-        # print("Initial best fitness = %.5f" % Fbest)
-        while t < self.MAX_ITERATIONS:
-
-            a = 2 * (1 - t / self.MAX_ITERATIONS)
-            a2 = -1 + t * ((-1)/self.MAX_ITERATIONS)
-
-            i = 0
-            for agent in agents:
-                A = 2 * a * random.random() - a
-                C = 2 * random.random()
-                b = 1
-                l = (a2-1)*random.random()+1
-
-                D = [0.0 for k in range(agent_dimension)]
-                D1 = [0.0 for k in range(agent_dimension)]
-                Xnew = [0.0 for k in range(agent_dimension)]
-                Xrand = [0.0 for k in range(agent_dimension)]
-                p = random.random()
-                r = random.random()
-
-                if p < 0.5:
-                    if abs(a) >= 1: # search for prey
-                        p = random.randint(0, self.AGENT_COUNT-1)
-                        while (p==i):
-                            p = random.randint(0, self.AGENT_COUNT-1)
-
-                        Xrand = agents[p]
-
-                        for j in range(agent_dimension):
-                            D[j] = abs(C * Xrand[j] - agent[j])
-                            Xnew[j] = Xrand[j] - A * D[j]
-                    else: # encircling prey
-                        if r < 0.5:
-                            for j in range(agent_dimension):
-                                D[j] = abs(C * Xrand[j] - agent[j])
-                                Xnew[j] = Xrand[j] - A * D[j]
-                        else:
-                            Xnew = vns.vns(agent)
-                else: # bubble net attacking
-                    if r < 0.5:
-                        for j in range(agent_dimension):
-                            D1[j] = abs(Xbest[j] - agent[j])
-                            Xnew[j] = D1[j] * math.exp(b * l) * math.cos(2 * math.pi * l) + Xbest[j]
-                    else:
-                        Xnew = vns.vns(agent)
-
-                for j in range(agent_dimension):
-                    agent[j] = Xnew[j]
-                i += 1
-
-            for i in range(len(agents)):
-                # jika Xnew < minx atau Xnew > maxx
-                for j in range(agent_dimension):
-                    agents[i][j] = max(agents[i][j], min_x)
-                    agents[i][j] = min(agents[i][j], max_x)
-
-                fitness_values[i] = self.fitness_function(agents[i])
-
-                if (fitness_values[i] > Fbest):
-                    Xbest = copy.copy(agents[i])
-                    Fbest = fitness_values[i]
-
-            # print("Iteration = " + str(t) + " | best fitness = %.5f" % Fbest)
-            t += 1
-        return Xbest, Fbest
-
+        return output['results'], fitness_value
+        return model.g_best.target.fitness
 
 class WOA_TSP(WOA_VRP):
     def fitness_function(self, agent):
@@ -555,110 +496,8 @@ class WOA_TSP(WOA_VRP):
             t += 1
         return Xbest, Fbest
 
-
-class WOA_VNS_TSP(WOA_VRP):
-    def fitness_function(self, agent):
-        routes = self.greedy_separate_route(agent, self.PREFERENCE_ID, self.DAYS_COUNT)
-        return self.get_multi_day_duration(routes)
-
-    def get_best_agent(self, agents):
-        index = -1
-        max = 0
-        i = 0
-        for agent in agents:
-            fitness_value = self.fitness_function(agent)
-            if fitness_value < max:
-                max = fitness_value
-                index = i
-            i += 1
-        return (max, agents[index])
-
-    def WOA(self, min_x, max_x, agents):
-        # setup VNS
-        vns = VNS(1, self.fitness_function, True)
-
-        t = 0
-
-        # Fbest : nilai fitness terbaik
-        # Xbest : agen terbaik
-        Fbest, Xbest = self.get_best_agent(agents)
-        agent_dimension = int(len(Xbest) / self.DAYS_COUNT) # banyak tempat wisata sesuai preferensi user
-        fitness_values = []
-
-        # Menyimpan nilai fitness untuk setiap agen
-        for i in range(len(agents)):
-            fitness_values.append(self.fitness_function(agents[i]))
-
-        # print("Initial best fitness = %.5f" % Fbest)
-        while t < self.MAX_ITERATIONS:
-
-            a = 2 * (1 - t / self.MAX_ITERATIONS)
-            a2 = -1 + t * ((-1)/self.MAX_ITERATIONS)
-
-            i = 0
-            for agent in agents:
-                A = 2 * a * random.random() - a
-                C = 2 * random.random()
-                b = 1
-                l = (a2-1)*random.random()+1
-
-                D = [0.0 for k in range(agent_dimension)]
-                D1 = [0.0 for k in range(agent_dimension)]
-                Xnew = [0.0 for k in range(agent_dimension)]
-                Xrand = [0.0 for k in range(agent_dimension)]
-                p = random.random()
-                r = random.random()
-                if p < 0.5:
-                    if abs(a) >= 1: # search for prey
-                        p = random.randint(0, self.AGENT_COUNT-1)
-                        while (p==i):
-                            p = random.randint(0, self.AGENT_COUNT-1)
-
-                        Xrand = agents[p]
-
-                        for j in range(agent_dimension):
-                            D[j] = abs(C * Xrand[j] - agent[j])
-                            Xnew[j] = Xrand[j] - A * D[j]
-                    else:  # encircling prey
-                        if r < 0.5:
-                            for j in range(agent_dimension):
-                                D[j] = abs(C * Xrand[j] - agent[j])
-                                Xnew[j] = Xrand[j] - A * D[j]
-                        else:
-                            Xnew = vns.vns(agent)
-                else:  # bubble net attacking
-                    if r < 0.5:
-                        for j in range(agent_dimension):
-                            D1[j] = abs(Xbest[j] - agent[j])
-                            Xnew[j] = D1[j] * math.exp(b * l) * math.cos(2 * math.pi * l) + Xbest[j]
-                    else:
-                        Xnew = vns.vns(agent)
-
-                for j in range(agent_dimension):
-                    agent[j] = Xnew[j]
-                i += 1
-
-            for i in range(len(agents)):
-                # jika Xnew < minx atau Xnew > maxx
-                for j in range(agent_dimension):
-                    agents[i][j] = max(agents[i][j], min_x)
-                    agents[i][j] = min(agents[i][j], max_x)
-
-                fitness_values[i] = self.fitness_function(agents[i])
-
-                if (fitness_values[i] < Fbest):
-                    Xbest = copy.copy(agents[i])
-                    Fbest = fitness_values[i]
-
-            # print("Iteration = " + str(t) + " | best fitness = %.5f" % Fbest)
-            t += 1
-        return Xbest, Fbest
-
-
-# w = WOA_VRP(['37', '22', '16', '18', '36', '10', '5', '17', '96', '12', '6', '94', '44', '40', '24', '2', '39', '20', '8', '26'], 129, 1, 0.5, 0.5, 3)
+# w = WOA_TSP(['37', '22', '16', '18', '36', '10', '5', '17', '96', '12', '6', '94', '44', '40', '24', '2', '39', '20', '8', '26'], 129, 1, 0.5, 0.5, 3)
 # w.prepare_dataset()
 
-# duration = w.construct_solution()
-# print(duration)
-
-#%%
+# fitness_values = w.construct_solution()
+# print(fitness_values)
